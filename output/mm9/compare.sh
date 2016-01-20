ab99e22361e6a1acc4ba395474656c12367680f6
Barna Zajzon <barna.zajzon@gmail.com>
2016-01-20 19:06:50 +0100

    updated merge_time and compare

--- output/mm9/compare.sh
+++ output/mm9/compare.sh
@@ -4,15 +4,17 @@ cd inverted
 for f in *.out; do
     temp1="../${f}.sorted.temp1"
     temp2="../${f}.sorted.temp2"
-    temp3="../${f}.sorted.temp3"
     sort -k1 -n $f > $temp1
     sort -k1 -n ../original/$f > $temp2
-    sort -k1 -n ../original-local/$f > $temp3
-    cmp --silent $temp1 $temp2 || echo "mismatch1 in ${f}" || error=1
-    cmp --silent $temp1 $temp3 || echo "mismatch2 in ${f}" || error=1
-    rm $temp1 $temp2 $temp3
+    cmp --silent $temp1 $temp2
+    if [[ $? != 0 ]]; then
+        error=1
+    fi
+    rm $temp1 $temp2 
 done
 
-if [ $error == 0 ]; then
+if [[ $error == 0 ]]; then
     echo "No mismatches"
+else
+    echo "Mismatch. Check"
 fi
--- output/mm9/merge_time.sh
+++ output/mm9/merge_time.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
-if [ -f "summary.data" ]; then 
-    rm "summary.data"
+cd inverted
+DATA="../summary.data"
+if [ -f $DATA ]; then 
+    rm $DATA
 fi
 
-cd inverted
+echo -e "qgram\tcons_error\terror_rate\tORIGINAL\tINVERTED" >> $DATA
+echo "" >> $DATA
 
 for e in {5..10..1}; do
     for q in {1..5}; do
         for c in {1..3}; do
-            for FILE in "chr*q${q}*c${c}*e${e}*.log"; do
+            for FILE in $(find . -maxdepth 1 -name "chr*q${q}*c${c}*e${e}*.log.bkp"); do
                 FILE=$(echo $FILE | sed 's/\.\///')
                 FILE_ORIG="../original/${FILE}"
                 if ([ -f $FILE ] && [ -f $FILE_ORIG ]); then
-                    TRIPLEX_SEARCH=$(sed -n '70p' $FILE)
-                    echo $TRIPLEX_SEARCH
-                    exit
-                fi
+                    echo "" >> $DATA
+                    [[ $( grep "triplex search only" $FILE_ORIG ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    TS_ORG=${BASH_REMATCH[1]}              
+                    [[ $( grep "triplex search only" $FILE ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    TS_INV=${BASH_REMATCH[1]}              
 
-                exit 
+                    [[ $( grep "IO reading" $FILE_ORIG ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    IO_ORG=${BASH_REMATCH[1]}              
+                    [[ $( grep "IO reading" $FILE ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    IO_INV=${BASH_REMATCH[1]}              
+
+                    [[ $( grep "qgram-Finder" $FILE_ORIG ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    QF_ORG=${BASH_REMATCH[1]}              
+                    [[ $( grep "qgram-Finder" $FILE ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    QF_INV=${BASH_REMATCH[1]}              
+
+                    [[ $( grep "collectSeeds" $FILE_ORIG ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    CS_ORG=${BASH_REMATCH[1]}              
+                    [[ $( grep "collectSeeds" $FILE ) =~ ([0-9]*\.[0-9]+)\ seconds ]] 
+                    CS_INV=${BASH_REMATCH[1]}              
+
+                    echo -e "${q}\t${c}\t${e}\t${TS_ORG}\t${TS_INV}\t\ttotal search" >> $DATA
+                    echo -e "${q}\t${c}\t${e}\t${IO_ORG}\t${IO_INV}\t\tio" >> $DATA
+                    echo -e "${q}\t${c}\t${e}\t${CS_ORG}\t${CS_INV}\t\tcollectSeeds" >> $DATA
+                    echo -e "${q}\t${c}\t${e}\t${QF_ORG}\t${QF_INV}\t\tqgramFind" >> $DATA
+                fi
             done
-            exit
         done
     done
 done
