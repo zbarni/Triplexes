81c1047c29db77c2b3d1a446ed36d592f108f89a
Barna Zajzon <barna.zajzon@gmail.com>
2016-05-22 14:24:24 +0200

    some results

--- output/myers/run_tests.sh
+++ output/myers/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # IMPORTANT: must be run from output/mm9 or similar
 
-l=25
+l=15
 chr=1
 DATA_DIR="${PWD}/../../data"
 ROOT_DIR=$PWD/..
@@ -9,8 +9,10 @@ EXEC_CMD="${PWD}/../../triplexator/bin/triplexator"
 MYERS=false
 BRUTE=false
 TRUNCATED=0
-e=15
-c=1
+eLow=5
+eHigh=15
+cLow=1
+cHigh=1
 
 while getopts ":mbl:c:t:" opt; do
     case ${opt} in
@@ -37,52 +39,57 @@ RNA_TRUNCATED_FILE="${DATA_DIR}/rna/CDP_truncated.fa"
 MYERS_TMP="${ROOT_DIR}/myers.tmp"
 BRUTE_TMP="${ROOT_DIR}/brute.tmp"
 
+for ((e=$eLow;e<=$eHigh;e+=5)); do
+    for ((c=$cLow;c<=$cHigh;c+=1)); do
+
 # switch directory according to options
-if [ "$MYERS" = true ]; then
-    cd ${ROOT_DIR}/myers
-    if [[ "$TRUNCATED" = 0 ]]; then
-        DNA=$DNA_FILE
-        RNA=$RNA_FILE
-        OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}"
-    elif [[ "$TRUNCATED" = 1 ]]; then
-        DNA=$DNA_FILE
-        RNA=$RNA_TRUNCATED_FILE
-        OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}_truncated_fulldna"
-    elif [[ "$TRUNCATED" = 2 ]]; then
-        DNA=$DNA_TRUNCATED_FILE
-        RNA=$RNA_TRUNCATED_FILE
-        OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}_truncated"
-    fi
+        if [ "$MYERS" = true ]; then
+            cd ${ROOT_DIR}/myers
+            if [[ "$TRUNCATED" = 0 ]]; then
+                DNA=$DNA_FILE
+                RNA=$RNA_FILE
+                OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}"
+            elif [[ "$TRUNCATED" = 1 ]]; then
+                DNA=$DNA_FILE
+                RNA=$RNA_TRUNCATED_FILE
+                OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}_truncated_fulldna"
+            elif [[ "$TRUNCATED" = 2 ]]; then
+                DNA=$DNA_TRUNCATED_FILE
+                RNA=$RNA_TRUNCATED_FILE
+                OUTPUT="chr${chr}_myers_l${l}_c${c}_e${e}_truncated"
+            fi
 
-    echo "Running l: ${l}, c: ${c}, e: ${e}"
+            echo "Running l: ${l}, c: ${c}, e: ${e}"
 #    echo "$EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -i -v -l $l -c $c -e $e &> ${OUTPUT}.dbg"
-    $EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -i -fm 1 -v -l $l -c $c -e $e &> ${OUTPUT}.dbg
+            $EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -i -fm 1 -v -l $l -c $c -e $e &> ${OUTPUT}.dbg
 
-    sort ${OUTPUT}.tpx > ${MYERS_TMP} 
-    #rm ${OUTPUT}.debug
-fi
+            sort ${OUTPUT}.tpx > ${MYERS_TMP} 
+            #rm ${OUTPUT}.debug
+        fi
 
-if [ "$BRUTE" = true ]; then
-    cd "${ROOT_DIR}/mm9/original/"
-    if [[ "$TRUNCATED" = 0 ]]; then
-        DNA=$DNA_FILE
-        RNA=$RNA_FILE
-        OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}"
-    elif [[ "$TRUNCATED" = 1 ]]; then
-        DNA=$DNA_FILE
-        RNA=$RNA_TRUNCATED_FILE
-        OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}_truncated_fulldna"
-    elif [[ "$TRUNCATED" = 2 ]]; then
-        DNA=$DNA_TRUNCATED_FILE
-        RNA=$RNA_TRUNCATED_FILE
-        OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}_truncated"
-    fi
+        if [ "$BRUTE" = true ]; then
+            cd "${ROOT_DIR}/mm9/original/"
+            if [[ "$TRUNCATED" = 0 ]]; then
+                DNA=$DNA_FILE
+                RNA=$RNA_FILE
+                OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}"
+            elif [[ "$TRUNCATED" = 1 ]]; then
+                DNA=$DNA_FILE
+                RNA=$RNA_TRUNCATED_FILE
+                OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}_truncated_fulldna"
+            elif [[ "$TRUNCATED" = 2 ]]; then
+                DNA=$DNA_TRUNCATED_FILE
+                RNA=$RNA_TRUNCATED_FILE
+                OUTPUT="chr${chr}_brute_l${l}_c${c}_e${e}_truncated"
+            fi
 
-    echo "Running l: ${l}, c: ${c}, e: ${e}"
+            echo "Running l: ${l}, c: ${c}, e: ${e}"
 #    echo "$EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -v -l $l -c $c -e $e &> ${OUTPUT}.dbg"
-    $EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -v -l $l -c $c -e $e &> ${OUTPUT}.dbg
-    sort ${OUTPUT}.tpx > ${BRUTE_TMP}
-fi
+            $EXEC_CMD -ss $RNA -ds $DNA -o ${OUTPUT}.tpx -v -l $l -c $c -e $e &> ${OUTPUT}.dbg
+            sort ${OUTPUT}.tpx > ${BRUTE_TMP}
+        fi
+    done
+done
 
 if [ ! -f ${MYERS_TMP} ]; then
     echo "Myers sorted output not found. Can't compare."
